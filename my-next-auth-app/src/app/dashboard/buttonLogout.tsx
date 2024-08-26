e77f4bb8d9d9b708a720afb2f993c66bd796a6a6
mateuswalz <mateuswalz@weg.net>
2024-08-26 08:23:23 -0300

    Corrige login do github

--- my-next-auth-app/src/app/dashboard/buttonLogout.tsx
+++ my-next-auth-app/src/app/dashboard/buttonLogout.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { Button } from "@mui/material";
 import { signOut } from "next-auth/react";
 
 const ButtonLogout = () => {
   return (
     <>
-      <Button variant="contained" onClick={() => signOut()}>
+      <Button variant="contained" onClick={() => signOut({ callbackUrl: "/" })}>
         Logout
       </Button>
     </>
